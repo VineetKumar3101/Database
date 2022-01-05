60859381e0f0ac63fc5ee825f1dc3d2443dcd561
Vineet Kumar <vineet.kumar_cs20@gla.ac.in>
2022-01-05 13:11:35 +0530

    Initial commit

---
+++ app/src/main/java/com/example/vineet/database/Third.java
@@ -0,0 +1,27 @@
+package com.example.vineet.database;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Third extends AppCompatActivity {
+Button b1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_third);
+        b1=(Button)findViewById(R.id.button5);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                Intent n=new Intent(Third.this,MainActivity.class);
+                startActivity(n);
+                finish();
+
+            }
+        });
+    }
+}
